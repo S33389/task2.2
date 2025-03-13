0918411fe4c9cde2ccca9cf8d36ad477b100470e
Marceli Lehmann <s33389@pjwstk.edu.pl>
2025-03-13 14:48:46 +0100

    task 1 done in main.java

--- zad2.2/src/Main.java
+++ zad2.2/src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double price =40.0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Enter your residence: ");
+        String residence = sc1.nextLine();
+        System.out.print("Enter the day you bought the ticket: ");
+        Scanner sc2 = new Scanner(System.in);
+        String day = sc1.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double discount = 0.0;
+
+        if(age<10){
+            price = 0;
+            discount=1;
         }
+        else if(age<=18){
+            discount = 0.5;
+        }
+
+        if(residence=="Warsaw"){
+            discount+=0.1;
+        }
+
+        if(day=="Thursday"){
+            price=0;
+            discount=1;
+        }
+
+        System.out.println("Data: " + residence +", " + age + " years old, " + day + " ticket price: " + (1.0-discount)*price + " PLN");
+        System.out.println("Discount: " + discount*100 + "%");
     }
 }
